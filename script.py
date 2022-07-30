75fc78cd21684969ca010c0b38186faf0fa7c70d
mnurbait <108135481+mnurbait@users.noreply.github.com>
2022-07-30 12:25:31 +0700

    Update script.py

--- script.py
+++ script.py
@@ -1,12 +1,9 @@
 import Adafruit_DHT
  
 sensor=Adafruit_DHT.DHT11
+pin=17
  
-gpio=17
- 
-humidity, temperature = Adafruit_DHT.read_retry(sensor, gpio)
- 
-if humidity is not None and temperature is not None:
-  print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity))
-else:
-  print('Failed to get reading. Try again!')
+while True:
+
+     humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+     print('Temp={0:0.1f}*C Humidity={1:0.1f}%'.format(temperature, humidity))
